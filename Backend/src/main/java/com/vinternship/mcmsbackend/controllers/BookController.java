653525df056341ded7b70bddb0dbc87facb4c3f6
tanvirkse101 <tanvirkse@gmail.com>
2022-04-25 16:09:49 +0600

    codebase setup

---
+++ Backend/src/main/java/com/vinternship/mcmsbackend/controllers/BookController.java
@@ -0,0 +1,125 @@
+package com.vinternship.mcmsbackend.controllers;
+
+import com.vinternship.mcmsbackend.models.Book;
+import com.vinternship.mcmsbackend.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api")
+public class BookController {
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> getAllBooks(@RequestParam(required = false) String name) {
+        try {
+            List<Book> books = new ArrayList<Book>();
+
+            if (name == null) {
+                books.addAll(bookRepository.findAll());
+            } else {
+                books.addAll(bookRepository.findBooksByTitleContaining(name));
+            }
+
+            if (books.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable("id") String id) {
+        Optional<Book> bookData = bookRepository.findById(id);
+
+        if (bookData.isPresent()) {
+            return new ResponseEntity<>(bookData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        try {
+            Book _book = bookRepository.save(new Book(
+                    book.getTitle(),
+                    book.getLanguage(),
+                    book.getYear(),
+                    book.getPages(),
+                    book.getAuthor(),
+                    book.getCountry(),
+                    book.getLink(),
+                    book.getImageLink()
+            ));
+            return new ResponseEntity<>(_book, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable("id") String id, @RequestBody Book book) {
+        Optional<Book> bookData = bookRepository.findById(id);
+
+        if (bookData.isPresent()) {
+            Book _book = bookData.get();
+            _book.setTitle(book.getTitle());
+            _book.setLanguage(book.getLanguage());
+            _book.setYear(book.getYear());
+            _book.setPages(book.getPages());
+            _book.setAuthor(book.getAuthor());
+            _book.setCountry(book.getCountry());
+            _book.setLink(book.getLink());
+            _book.setImageLink(book.getImageLink());
+            return new ResponseEntity<>(bookRepository.save(_book), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<HttpStatus> deleteBook(@PathVariable("id") String id) {
+        try {
+            bookRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/books")
+    public ResponseEntity<HttpStatus> deleteAllBooks() {
+        try {
+            bookRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/books/title/{name}")
+    public ResponseEntity<List<Book>> findByTitle(@PathVariable("name") String name) {
+        try {
+            List<Book> books = bookRepository.findBooksByTitleContaining(name);
+
+            if (books.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ Backend/src/main/java/com/vinternship/mcmsbackend/models/Book.java
@@ -0,0 +1,120 @@
+package com.vinternship.mcmsbackend.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "books")
+public class Book {
+    @Id
+    private String id;
+    private String title;
+    private String language;
+    private int year;
+    private int pages;
+    private String author;
+    private String country;
+    private String link;
+    private String imageLink;
+
+    // Empty constructor
+    public Book() {
+    }
+
+    // Constructor
+    public Book(String title, String language, int year, int pages, String author,
+                String country, String link, String imageLink) {
+        this.title = title;
+        this.language = language;
+        this.year = year;
+        this.pages = pages;
+        this.author = author;
+        this.country = country;
+        this.link = link;
+        this.imageLink = imageLink;
+    }
+
+    // Getters & setters
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    // To string method
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", language='" + language + '\'' +
+                ", year=" + year +
+                ", pages=" + pages +
+                ", author='" + author + '\'' +
+                ", country='" + country + '\'' +
+                ", link='" + link + '\'' +
+                ", imageLink='" + imageLink + '\'' +
+                '}';
+    }
+}
